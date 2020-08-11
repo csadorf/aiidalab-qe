d0a631693f0eb80757438091c982dbfd2453afed
Simon Adorf <simon.adorf@epfl.ch>
2020-08-11 17:44:31 +0000

    (WIP) Prepare QE app for app-environments feature.

--- start.py
+++ start.py
@@ -1,17 +1,15 @@
 import ipywidgets as ipw
 from IPython.core.display import HTML
+from aiidalab.utils import url_for
 
 def get_start_widget(appbase, jupbase, notebase):
     #http://fontawesome.io/icons/
-    template ="""
+    return ipw.HTML(f"""
     <div align="center">
-        <a href="{appbase}/qe.ipynb" target="_blank">
+        <a href="{url_for(appbase + 'qe.ipynb')}" target="_blank">
             <img src="https://gitlab.com/QEF/q-e/raw/develop/logo.jpg" height="120px" width=243px">
         </a>
     </div>
-    """
-    
-    html = template.format(appbase=appbase, jupbase=jupbase, notebase=notebase)
-    return ipw.HTML(html)
+    """)
     
 #EOF
